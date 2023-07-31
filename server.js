3b04b46736e7e6eddbb4cee7c92cfaf4fde08c5f
Willie Lu <Willie_Lu@macadmin-tws-Mac-mini.local>
2023-07-31 17:39:22 +0800

    update

--- server.js
+++ server.js
@@ -19,84 +19,43 @@ const {Builder, Browser, By, Key, until, locateWith} = require('selenium-webdriv
   try {
     await driver.get('https://www.skyscanner.com.hk/transport/flights/hkg/es/?adultsv2=1&cabinclass=economy&childrenv2=&ref=home&rtn=1&preferdirects=false&outboundaltsenabled=false&inboundaltsenabled=false&oym=2308&iym=2308&qp_prevScreen=COMBINED_EXPLORE');
 
-    // card = await driver.findElement(By.id('app-root'));
-    // const card = await driver.findElements(By.className('ResultList_placeCard__NDZmM'));
-
-
-    // BpkCard_bpk-card__OGNlM
     
     await driver.manage().setTimeouts({implicit: 100000});
-    // let title = await driver.getTitle();
-    // container = driver.findElement(By.xpath("//*[starts-with(@class, 'ResultList_container')]"));
-    // cards = await container.findElements(By.xpath("//*[starts-with(@class, 'BpkCard_bpk-card__OGNlM')]"));
-    // cards = await container.findElements(By.xpath("//*[starts-with(@class, 'PlaceCard_descriptionContainer')]"));
 
     cards = await driver.wait(until.elementsLocated(By.css('[class^="PlaceCard_descriptionContainer"]')));
-    // cards =  await driver.findElements(By.css('[class^="PlaceCard_descriptionContainer"]'));;
-    
-    // const cards = await driver.findElements(By.xpath("//*[starts-with(@class, 'PlaceCard_descriptionContainer')]"));
-    
-    // console.log(cards);
-
-    // cards = locateWith(By.xpath("//*[starts-with(@class, 'ResultList_container')]")).below(By.xpath("//*[starts-with(@class, 'BpkCard_bpk-card__OGNlM')]"));
-    // cards = locateWith(By.xpath("//*[starts-with(@class, 'BpkCard_bpk-card__OGNlM')]")).below(By.xpath("//*[starts-with(@class, 'ResultList_container')]"));
-    
-    // console.log(cards);
     let flightTable = [];
 
     for (const card of cards) {
       divs = await card.findElements(By.css("div"));
+        let divIndex = 0;
+        let row = [];
       for (const div of divs) {
-        // console.log());
         spans = await div.findElements(By.css("span"));
-        // console.log(spans);
         
         let spanIndex = 0;
-        let row = [];
 
         for(const span of spans) {
-          console.log('///////');
-          console.log(spanIndex);
-          console.log(await span.getText());
+          // console.log('///////');
+          // console.log(spanIndex);
+          // console.log(await span.getText());
 
-
-          if(spanIndex !== 1){
-            row.push(await span.getText());
+          console.log(divIndex, spanIndex);
+t = await span.getText();
+          console.log(t)
+          if((divIndex !== 1) && (spanIndex !== 0)){
+            row.push(t);
           } 
           spanIndex++;
 
         }
-        // flightTable.push(row);
+        divIndex++;
       }
+        flightTable.push(row);
 
 
-      // Extract the location name
-      // const locationName = await card.findElement(By.className('BpkText_bpk-text--heading-4__NDQ1Y')).getText();
-
-      // Extract the price
-      // const price = await card.findElement(By.className('BpkText_bpk-text--heading-4__NDQ1Y')).getText();
-
-      // Extract the number of connecting flights
-      // const connectingFlightsText = await card.findElement(By.className('BpkText_bpk-text--caption__NTZiO')).getText();
-      // const numberOfConnectingFlights = parseInt(connectingFlightsText.split(' ')[1]);
-
-      // Print the extracted information
-      // console.log('Location: ', locationName);
-      // console.log('Price: ', price);
-      // console.log('Number of Connecting Flights: ', numberOfConnectingFlights);
     }
     console.table(flightTable);
-    // card = driver.findElement(By.className("BpkCard_bpk-card__OGNlM"));
-
-     
-    // let textBox = await driver.findElement(By.name('my-text'));
-    // let submitButton = await driver.findElement(By.css('button'));
-    // await textBox.sendKeys('Selenium');
-    // await submitButton.click();
-    // await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
-    // await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
   } finally {
     // await driver.quit();
   }
 })();
-// 终端打印如下信息
